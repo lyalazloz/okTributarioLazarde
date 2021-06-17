e8dbaf9c46a837736aad04a2b0230d774a495c95
lyalazloz <lyalazloz@gmail.com>
2021-06-17 03:24:48 -0300

    unificacion repositorios

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {NavBar} from './components/navBar/navBar'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <h1>hola</h1>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,11 @@
+export const NavBar = () => {
+    return (
+      <nav>
+       <ul>
+           <li>Cursos Gratuitos</li>
+           <li>Cursos Premium</li>
+           <li>Charlas Virtuales</li>
+       </ul>
+      </nav>
+    )
+}
