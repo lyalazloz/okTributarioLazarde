12e4368c670cd6d4fd0b313d98f027a0c22c574e
lyalazloz <lyalazloz@gmail.com>
2021-06-24 17:40:27 -0300

    initial navBar.css

--- src/components/cartWidget/cartWidget.js
+++ src/components/cartWidget/cartWidget.js
@@ -1,6 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const cart= <FontAwesomeIcon icon={faShoppingCart} size="2x" inverse/>
+
 export const CartWidget = () => {
     return (    
-        <div>🛒︁</div>        
+        <div className= "cart">{cart}︁</div>        
     )
   }
 
--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,25 +1,40 @@
 import {CartWidget} from '../cartWidget/cartWidget'
 import logo from '../navBar/logoOk.PNG'
+import './navBar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
+
+const chevron = <FontAwesomeIcon icon={faChevronDown} />
 
 export const NavBar = () => {
-  return (      
-    <nav>
-      <img src={logo} alt="logo-ok-tributario"/>
-      <ul>
-        <li><a href="#">Inicio</a></li>
-        <li><a href="#">Capacitaciones</a>
-            <ul>
-              <li><a href="#">Capacitaciones Premium</a></li>
-              <li><a href="#">Capacitaciones Gratuitas</a></li>
-            </ul>
-        </li>
-        <li><a href="#">Nosotros</a></li>
-        <li><a href="#">Publicaciones</a></li>
-        <li><a href="#">Conectemos</a></li>
-      </ul>
-      <CartWidget/>
-    </nav>
-    
-
+  return (    
+    <div className="navbar">
+      <i className="fa fa-bars bars-button" aria-hidden="true" id="bars-button"></i>
+      <div className="contenido">
+        <nav className="bars">
+          <div><img src={logo} alt="logo-ok-tributario"/></div>          
+          <ul>
+            <li><a href="#">Inicio</a></li>
+              <ul>
+                <div class="dropdown">
+                  <button class="dropbtn">Capacitaciones {chevron}
+                  </button>
+                  <div class="dropdown-content">
+                    <li><a href="#">Capacitaciones Premium</a></li>
+                    <li><a href="#">Capacitaciones Gratuitas</a></li>
+                  </div>
+                </div>
+              </ul> 
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="#">Publicaciones</a></li>
+            <li><a href="#">Conectémos</a></li>            
+          </ul> 
+        </nav>
+        <a href="#"><CartWidget/></a>
+      </div>
+    </div>
   )
 }
+
+
